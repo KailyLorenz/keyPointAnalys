94c8e5331375174abda98f99751a9693e5b1f56e
Kaily <kaily.lorenz@gmail.com>
2024-01-31 12:59:01 +0200

    deploy Node

--- app.js
+++ app.js
@@ -43,17 +43,17 @@ app.use('/api/v1/users', userRouter)
 
 app.use(globalErrorHandler)
 // for deploy
-app.use(express.static(path.join(__dirname, '../client')));
-app.get('/*', (req, res) => {
-  res.sendFile(
-      path.join(__dirname, '../client', 'index.html')
-  );
-});
+// app.use(express.static(path.join(__dirname, '../client')));
+// app.get('/*', (req, res) => {
+//   res.sendFile(
+//       path.join(__dirname, '../client', 'index.html')
+//   );
+// });
 
-app.use(express.static(path.join(__dirname, '../client/src')))
+app.use(express.static(path.join(__dirname, '../client')))
 app.get('*', (req, res) => {
   res.sendFile(
-      './index.html'
+      './frontend.html'
   )
 })
 if (process.env.NODE_ENV === 'production') {
