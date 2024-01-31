4818ce8bf020a0f8d50548ba332f0fd62126423c
Kaily <kaily.lorenz@gmail.com>
2024-01-31 11:54:46 +0200

    deploy Node

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config({path: `${__dirname}/config.env`})
 const app = require('./app')
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, "0.0.0.0", function (){
+const server = app.listen(port,  () =>{
   console.log(`App running on port ${port}`)
 })
 
