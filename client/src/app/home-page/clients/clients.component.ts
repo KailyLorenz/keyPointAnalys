6c246494844b76cb621e76e072c761edbc54b7c4
Kaily <kaily.lorenz@gmail.com>
2024-04-05 16:40:10 +0300

    deploy 05/04-06 fix mobile version

--- client/src/app/home-page/clients/clients.component.ts
+++ client/src/app/home-page/clients/clients.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ClientsComponent {
   clients = [
-    {'src': 'assets/img/clients/bitrix-logo.jpg', 'alt': 'bitrix-logo'},
-    {'src': 'assets/img/clients/wildberries-logo.jpg', 'alt': 'microsoft-sql-server'},
+    {'src': 'assets/img/clients/bitrix-logo.png', 'alt': 'bitrix-logo'},
+    {'src': 'assets/img/clients/wildberries-logo.png', 'alt': 'microsoft-sql-server'},
+    {'src': 'assets/img/clients/ozon-logo.png', 'alt': 'ozon-logo'},
+    {'src': 'assets/img/clients/excel-logo.jpg', 'alt': 'my_warehouse'},
     {'src': 'assets/img/clients/my_warehouse.jpg', 'alt': 'my_warehouse'},
-    // {'src': 'assets/img/clients/power_bi.jpg', 'alt': 'power_bi'},
-    {'src': 'assets/img/clients/my_warehouse.jpg', 'alt': 'my_warehouse'},
-    {'src': 'assets/img/clients/1c-logo.jpg', 'alt': '1c-logo'},
-    {'src': 'assets/img/clients/ozon-logo.jpg', 'alt': 'ozon-logo'},
+    {'src': 'assets/img/clients/1c-logo.png', 'alt': '1c-logo'},
+
   ]
 }
