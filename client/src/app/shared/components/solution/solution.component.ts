2fb8edbf32288d4d0a0c4c1380a7ac8d9ab025d5
Kaily <kaily.lorenz@gmail.com>
2024-05-19 19:56:10 +0300

    deploy 19/05.4

---
+++ client/src/app/shared/components/solution/solution.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UtilsService} from "../../services/utils.service";
+
+@Component({
+  selector: 'app-about-detail',
+  templateUrl: './solution.component.html',
+  styleUrls: ['./solution.component.css']
+})
+export class SolutionComponent implements OnInit {
+  type!: string
+
+  constructor(private utilsService: UtilsService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // this.utilsService.setSubHeader(true)
+    this.route.params.subscribe(params => {
+      this.type = params['type']
+      console.log('type', this.type)
+    })
+  }
+}
